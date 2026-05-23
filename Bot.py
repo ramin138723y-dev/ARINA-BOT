ebf3811d3ceb3bc8bd41214b608d883dc2666ebb
ramin138723y-dev <ramin1387.23y@gmail.com>
2026-05-23 08:14:30 +0330

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,36 @@
+import telebot
+import threading
+import time
+import schedule
+
+TOKEN = "8697245449:AAGtpJ9CQHPnXDwqQQai-c1aQO0DXr9cv_s"
+
+bot = telebot.TeleBot(TOKEN)
+
+users = set()
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    users.add(message.chat.id)
+
+    bot.send_message(message.chat.id, "❤️")
+
+def send_heart():
+    for user_id in users:
+        try:
+            bot.send_message(user_id, "❤️")
+        except:
+            pass
+
+schedule.every().day.at("00:00").do(send_heart)
+
+def scheduler():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+threading.Thread(target=scheduler).start()
+
+print("Bot is running...")
+
+bot.infinity_polling()
